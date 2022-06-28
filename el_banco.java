809969a70ac9355acdffd323b434e107ffbcd0ce
PowerlineR413 <Powerline413@yandex.ru>
2022-06-28 14:31:22 +0500

    El banco task

---
+++ el_banco.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class el_banco
+{
+    public static void main(String[] args)
+    {
+        int a = 13;
+        String b = "Добро пожаловать в интерфейс программы подсчёта " +"\n" +
+                "процентов по вкладу Подсчётоматик (v0.9) " +"\n" +
+                "Ваш годовой процент по новому вкладу в рублях составляет " + a + " % годовых. " + "\n"+
+                "Пожалуйста, введите цифрами сумму Вашего вклада, и нажмите ENTER " + "\n" +
+                "для подсчёта процентов за два года - это минимальный срок вложения средств. ";
+        System.out.println(b);
+        Scanner sc = new Scanner(System.in);
+        int vklad = sc.nextInt();
+        double prs = (vklad / 100) * 13 * 2;
+        double marga = vklad + prs;
+        String itog = "Ваш процент по вкладу в сумме " +vklad+ " рублей за два года " +
+                "составит " +prs+" рублей." +"\n" +
+                "Общая сумма вашего вклада таким образом составит " +marga+" рублей";
+        System.out.println(itog);
+        sc.close();
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
